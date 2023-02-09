a0fb02b4cacd14814559f1fb83dc60a186fa4bf5
Zifan He <zifanhe1202@g.ucla.edu>
2023-02-09 01:16:55 -0800

    add read-only mmap

--- run_tapa.sh
+++ run_tapa.sh
@@ -4,6 +4,12 @@ tapac \
     --top TrigSolver \
     --work-dir solver.xilinx_u280_xdma_201920_3.hw.xo.tapa \
     --connectivity hbm_config.ini \
+    --read-only-args csr_edge_list_ch* \
+    --read-only-args csr_edge_list_ptr* \
+    --read-only-args csc_col_ptr* \
+    --read-only-args csc_row_ind* \
+    --read-only-args f* \
+    --read-only-args K_csc* \
     --enable-hbm-binding-adjustment \
     --enable-synth-util \
     --run-floorplan-dse \
