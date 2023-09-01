274eda30009fbee3933bbd32b853a9a19d0da7a8
Oswald He <zifanhe1202@ucla.edu>
2023-09-01 10:40:09 -0700

    use Vitis 2019

--- run_tapa.sh
+++ run_tapa.sh
@@ -1,8 +1,8 @@
 tapac \
-    -o solver.xilinx_u280_gen3x16_xdma_1_202211_1.hw.xo \
-    --platform xilinx_u280_gen3x16_xdma_1_202211_1 \
-     --top TrigSolver \
-    --work-dir solver.xilinx_u280_gen3x16_xdma_1_202211_1.hw.xo.tapa \
+    -o solver.xilinx_u280_xdma_201920_3.hw.xo \
+    --platform xilinx_u280_xdma_201920_3 \
+    --top TrigSolver \
+    --work-dir solver.xilinx_u280_xdma_201920_3.hw.xo.tapa \
     --connectivity hbm_config.ini \
     --read-only-args csr_edge_list_ch* \
     --read-only-args dep_graph_ch* \
